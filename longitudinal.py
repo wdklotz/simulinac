1f277e103f7b869105b3e5854937923070647af5
Wolf-Dieter Klotz <wdklotz@gmail.com>
2015-05-09 17:16:48 +0200

    more logitudinal.py

--- longitudinal.py
+++ longitudinal.py
@@ -4,6 +4,13 @@ from setup import Phys,Proton
 from pylab import plot,show,legend,figure,subplot,axis
 from math import pi,cos
 
+def display(functions):
+    for func in functions:
+        offp=  [x[0] for x in func]
+        offw=  [x[1] for x in func]
+        plot(offp,offw,label=r'$\Delta$w($\Delta$phi)')
+    # legend(loc='lower right',fontsize='x-small')
+    show()
 def phase(factor,ws,w):
     return  (w-ws)*factor
 def energy(factor,ps,p):
@@ -18,25 +25,27 @@ def loop(phis0,ws0,phi0,w0):
     pr=Proton(ws); mc2=pr.e0; g=pr.gamma; b=pr.beta; phase_factor = -2*pi/(mc2*g*g*g*b*b)
     T=pr.TrTf(); energy_factor=Phys['spalt_spannung']*T
 
-    functions=[]
+    func=[]
     for i in range(70):
         # print('{}   (ps,ws)= ({:5.3f},{:5.3f})  (p,w)= ({:5.3f},{:5.3f})'.format(i,ps*deg,ws,p*deg,w))
         w = w + energy(energy_factor,ps,p)
         p = p + phase(phase_factor,ws,w)
-        functions.append([i,(ps-p)*deg,w-ws])
-    bin=   [x[0] for x in functions]
-    offp=  [x[1] for x in functions]
-    offw=  [x[2] for x in functions]
-    plot(offw,offp,label=r'$\Delta$w($\Delta$phi)')
-    legend(loc='lower right',fontsize='x-small')
-    show()
-#-----------*-----------*-----------*-----------*-----------*-----------*-----------*
-if __name__ == '__main__':
-    phis0=Phys['radians']*-90.
-    phi0 =Phys['radians']*-95.
+        fun=((ps-p)*deg,w-ws)
+        func.append(fun)
+    return func
+def test0():
+    rad = Phys['radians']
+    phis0=-30.
     ws0=50.
     w0 =ws0
-    loop(phis0,ws0,phi0,w0)
-        
+    functions=[]
+    for i in [phis0+n*4.775065 for n in range(20)]:
+        phi0 = i*rad
+        func= loop(phis0*rad,ws0,phi0,w0)
+        functions.append(func)
+    display(functions)
+#-----------*-----------*-----------*-----------*-----------*-----------*-----------*
+if __name__ == '__main__':
+    test0()
         
         
