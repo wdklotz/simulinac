ac34d81f79a8c4fa0174a10b6d256088a8f10ce4
Wolf-Dieter Klotz <wdklotz@gmail.com>
2017-09-09 17:50:15 +0200

    bkp 9.9.17-17:49; Epoly in ez0.py OK now

--- Ez0.py
+++ Ez0.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from math import sin,cos,tan,pi,exp
+from collections import namedtuple
 
 from setutil import PARAMS,DEBUG,Proton
 
@@ -11,98 +12,14 @@ def NGauss(x,sig,mu):    # Gauss Normalverteilung
     res = exp(-(((x-mu)/sig)**2/2.))
     return res
 
-def Epoly(z,poly):
-    """
-    Plynomial fit
-    """
-    ix = -1
-    for i in range(len(poly)):
-        zl = poly[i][0]
-        z0 = poly[i][1]
-        zr = poly[i][2]
-        if zl <= z and z <= zr:
-            ix = i
-            break
-    if ix <0:
-        raise RuntimeError('arg out of range! {}'.format(z))
-    E0 = poly[ix][4]
-    a  = poly[ix][5]
-    b  = poly[ix][6]
-    dz = z-z0
-    res = E0*(1.+ a*dz+b*dz**2)
-    return res
-
-def V0(poly):
-    v0 = []
-    for i in range(len(poly)):
-        zl = poly[i][0]
-        z0 = poly[i][1]
-        E0 = poly[i][4]
-        b  = poly[i][6]
-        dz = z0-zl
-        v  = E0*(2*dz+2./3.*b*dz**3)
-        v0.append(v)
-    return v0
-
-def Tk(poly,k):
-    tk = []
-    for i in range(len(poly)):
-        zl = poly[i][0]
-        z0 = poly[i][1]
-        a  = poly[i][5]
-        b  = poly[i][6]
-        dz = z0-zl
-        rs = 2*sin(k*dz)/k/(2*dz+2./3.*b*dz**3)
-        rs = rs * (1.+b*dz**2-2*b/k**2+2*b*dz/k/tan(k*dz))
-        tk.append(rs)
-    return tk
-
-def Sk(poly,k):
-    sk = []
-    for i in range(len(poly)):
-        zl = poly[i][0]
-        z0 = poly[i][1]
-        a  = poly[i][5]
-        b  = poly[i][6]
-        dz = z0-zl
-        rs = 2*a*sin(k*dz)/k**2/(2*dz+2./3.*b*dz**3)
-        rs = rs * (1.-k*dz/tan(k*dz))
-        sk.append(rs)
-    return sk
-
-def Tkp(poly,k):
-    tkp = []
-    for i in range(len(poly)):
-        zl = poly[i][0]
-        z0 = poly[i][1]
-        a  = poly[i][5]
-        b  = poly[i][6]
-        dz = z0-zl
-        rs = 2*sin(k*dz)/k/(2*dz+2./3.*b*dz**3)
-        rs = rs * ((1.+3*b*dz**2-6*b/k**2)/k-dz/tan(k*dz)*(1.+b*dz**2-6*b/k**2))
-        tkp.append(rs)
-    return tkp
-
-def Skp(poly,k):
-    skp = []
-    for i in range(len(poly)):
-        zl = poly[i][0]
-        z0 = poly[i][1]
-        a  = poly[i][5]
-        b  = poly[i][6]
-        dz = z0-zl
-        rs = 2*a*sin(k*dz)/k/(2*dz+2./3.*b*dz**2)
-        rs = rs * (dz**2-2./k**2+2*dz/k/tan(k*dz))
-        skp.append(rs)
-    return skp
-
 def Kpoly(z,sigma,E):
     """
     Calculate polynom coefficients
     """
-    interval = []
-    anz = int((len(z)-1)/2)
-    for i in range(0,anz+2,2):
+    poly = []
+    Interval = namedtuple('Interval',['zl','z0','zr','dz','E0','a','b'])
+    anz = len(z)
+    for i in range(0,anz-2,2):
         zl  = z[i]
         z0  = z[i+1]
         zr  = z[i+2]
@@ -112,8 +29,84 @@ def Kpoly(z,sigma,E):
         Er  = E*NGauss(zr,sigma,0.)
         a   = (Er-El)/(2*E0*dz)
         b   = (Er+El-2*E0)/(2*E0*dz**2)
-        interval.append((zl,z0,zr,dz,E0,a,b))
-    return interval
+        interval = Interval(zl,z0,zr,dz,E0,a,b)
+        poly.append(interval)
+    return poly
+
+def Epoly(z,poly):
+    """
+    Plynomial fit
+    """
+    ix = -1
+    for i in range(len(poly)):
+        zl = poly[i].zl
+        z0 = poly[i].z0
+        zr = poly[i].zr
+        if zl <= z and z <= zr:
+            ix = i
+            break
+    if ix <0:
+        raise RuntimeError('arg out of range! {}'.format(z))
+    E0 = poly[ix].E0
+    a  = poly[ix].a
+    b  = poly[ix].b
+    dz = z-z0
+    res = E0*(1.+ a*dz+b*dz**2)
+    return res
+
+def V0(poly):
+    v0 = []
+    for i in range(len(poly)):
+        E0 = poly[i].E0
+        b  = poly[i].b
+        dz = poly[i].dz
+        v  = E0*(2*dz+2./3.*b*dz**3)
+        v0.append(v)
+    return v0
+
+def Tk(poly,k):
+    tk = []
+    for i in range(len(poly)):
+        a  = poly[i].a
+        b  = poly[i].b
+        dz = poly[i].dz
+        rs = 2*sin(k*dz)/k/(2*dz+2./3.*b*dz**3)
+        rs = rs * (1.+b*dz**2-2*b/k**2+2*b*dz/k/tan(k*dz))
+        tk.append(rs)
+    return tk
+
+def Sk(poly,k):
+    sk = []
+    for i in range(len(poly)):
+        a  = poly[i].a
+        b  = poly[i].b
+        dz = poly[i].dz
+        rs = 2*a*sin(k*dz)/k**2/(2*dz+2./3.*b*dz**3)
+        rs = rs * (1.-k*dz/tan(k*dz))
+        sk.append(rs)
+    return sk
+
+def Tkp(poly,k):
+    tkp = []
+    for i in range(len(poly)):
+        a  = poly[i].a
+        b  = poly[i].b
+        dz = poly[i].dz
+        rs = 2*sin(k*dz)/k/(2*dz+2./3.*b*dz**3)
+        rs = rs * ((1.+3*b*dz**2-6*b/k**2)/k-dz/tan(k*dz)*(1.+b*dz**2-6*b/k**2))
+        tkp.append(rs)
+    return tkp
+
+def Skp(poly,k):
+    skp = []
+    for i in range(len(poly)):
+        a  = poly[i].a
+        b  = poly[i].b
+        dz = poly[i].dz
+        rs = 2*a*sin(k*dz)/k/(2*dz+2./3.*b*dz**2)
+        rs = rs * (dz**2-2./k**2+2*dz/k/tan(k*dz))
+        skp.append(rs)
+    return skp
 
 def pre_plt():
     ax  = plt.subplot(111)
@@ -132,6 +125,11 @@ def display(table,legend):
     zn   = [-float(x[0]) for x in reversed(table)]
     Ezn  = [+float(x[2]) for x in reversed(table)]
     plt.plot(zn+zp,Ezn+Ezp,label=legend)
+
+def display1(table,legend):
+    z   = [+float(x[0]) for x in table]
+    Ez  = [+float(x[2]) for x in table]
+    plt.plot(z,Ez,label=legend)
 
 def test0():
     '''
@@ -181,36 +179,35 @@ def test2():
     freq     = PARAMS['frequenz']
     k        = 2*pi*freq/(c*beta)
 
-    anz   = 3            # nboff slices
+    anz   = 12           # nboff slices
     gap   = 4.4          #[cm] full gap length
-    zl    = 0.           #left  interval boundary
-    zr    = gap/2.       #right interval boundary
-    sigma = (zr-zl)/1.89 # sigma of NGauss (best fit with SF)
+    zl    = -gap/2.      #left  interval boundary
+    zr    = +gap/2.      #right interval boundary
+    sigma = zr/1.89      # sigma of NGauss (best fit with SF)
     E0    = 1.0          # top of NGauss   (best fit with SF)
 
-    zstep = (zr-zl)/(2*anz)
-    z = np.arange(zl,zr+zstep,zstep)
+    z = np.linspace(zl,zr,2*anz+1)
     # DEBUG('z',z)
-    # Ez0_tab = [(x,0.,E0*NGauss(x,sigma,0.)) for x in z]
-    # display(Ez0_tab,'slice')
+    Ez0_tab = [(x,0.,E0*NGauss(x,sigma,0.)) for x in z]
+    display1(Ez0_tab,'slice')
     poly = Kpoly(z,sigma,E0)
     # DEBUG('poly',poly)
 
     zstep = (zr-zl)/1000.
     z = np.arange(zl,zr,zstep)
     Ez0_tab = [(x,0.,Epoly(x,poly)) for x in z]
-    display(Ez0_tab,'poly')
+    display1(Ez0_tab,'poly')
 
     # TTF calculations
-    v0 = V0(poly)
-    DEBUG('v0',v0)
-    tk = Tk(poly,k)
-    DEBUG('T(k)',tk)
-    sk = Sk(poly,k)
-    DEBUG('S(k)',sk)
+    v0  = V0(poly)
+    tk  = Tk(poly,k)
+    sk  = Sk(poly,k)
     tkp = Tkp(poly,k)
-    DEBUG("T'(k)",tkp)
     skp = Skp(poly,k)
+    DEBUG('v0',   v0)
+    DEBUG('T(k)', tk)
+    DEBUG('S(k)', sk)
+    DEBUG("T'(k)",tkp)
     DEBUG("S'(k)",skp)
 
 if __name__ == '__main__':
