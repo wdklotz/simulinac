f88ffa588b2c54a34c04db92e9df4623dd15f69d
Wolf-Dieter Klotz <wdklotz@gmail.com>
2017-10-03 10:56:10 +0200

    tets/beta_proton.py again

--- tests/beta_proton.py
+++ tests/beta_proton.py
@@ -11,7 +11,8 @@ def BetaGamma(particle):
     gamma      = particle.gamma
     gamma_beta = particle.gamma_beta
     pc         = gamma_beta*particle.e0
-    return (vclassic, beta, gamma,gamma_beta,pc)
+    brho       = gamma_beta*3.1297
+    return (vclassic, beta, gamma,gamma_beta,pc,brho)
 
 print ('Proton Ruhemasse {:.3f} [Mev/c**2]'.format(Proton().e0))
 header = [
@@ -21,11 +22,12 @@ header = [
     'v(rel)[m/s]',
     'gamma',
     'gam*bet',
-    'pc[MeV]'
+    'pc[MeV]',
+    'B*rho[Tm]'
     ]
 rows   = []
 for tk in range(5,21,1):
-    vclassic,beta,gamma,gamma_beta,pc = BetaGamma(Proton(tkin=tk))
+    vclassic,beta,gamma,gamma_beta,pc,brho = BetaGamma(Proton(tkin=tk))
     row = [
         '{:.1f}'.format(tk),
         '{:10.4e}'.format(vclassic),
@@ -33,13 +35,14 @@ for tk in range(5,21,1):
         '{:10.4e}'.format(beta*c),
         '{:1.4f}'.format(gamma),
         '{:1.4f}'.format(gamma_beta),
-        '{:1.4f}'.format(pc)
+        '{:1.4f}'.format(pc),
+        '{:1.4f}'.format(brho)
         ]
     rows.append(row)
 print(tblprnt(header,rows))
 rows   = []
 for tk in range(20,205,5):
-    vclassic,beta,gamma,gamma_beta,pc = BetaGamma(Proton(tkin=tk))
+    vclassic,beta,gamma,gamma_beta,pc,brho = BetaGamma(Proton(tkin=tk))
     row = [
         '{:.1f}'.format(tk),
         '{:10.4e}'.format(vclassic),
@@ -47,7 +50,8 @@ for tk in range(20,205,5):
         '{:10.4e}'.format(beta*c),
         '{:1.4f}'.format(gamma),
         '{:1.4f}'.format(gamma_beta),
-        '{:1.4f}'.format(pc)
+        '{:1.4f}'.format(pc),
+        '{:1.4f}'.format(brho)
         ]
     rows.append(row)
 print(tblprnt(header,rows))
