1aa9b38e213b36393c24c3fb837144170b5d5509
Wolf-Dieter Klotz <wdklotz@gmail.com>
2017-10-11 12:11:02 +0200

    continue integration of SIXDR

--- sixtrack.py
+++ sixtrack.py
@@ -22,7 +22,7 @@ from math import sqrt
 import numpy as np
 from copy import copy
 
-from setutil import PARAMS,DEBUG
+from setutil import PARAMS,DEBUG,Proton,tblprnt
 from elements import XKOO,XPKOO,YKOO,YPKOO,ZKOO,ZPKOO,EKOO,DEKOO,SKOO,LKOO
 import elements as ELM
 
@@ -38,10 +38,10 @@ class SIXD(ELM.D):
     """
     Sixtrack drift space
     """
-    def __init__(self,length=0.,label="SIXD",viseeo=0.,particle=PARAMS['sollteilchen'],position=[0.,0.,0.]):
-        super().__init__(length=length, viseo=video, label='SIXD', particle=particle, position=position)
+    def __init__(self,length=0.,label="SIXD",viseo=0.,particle=PARAMS['sollteilchen'],position=[0.,0.,0.]):
+        super().__init__(length=length, viseo=viseo, label='SIXD', particle=particle, position=position)
         self.off_soll = copy(self.particle)
-   def shorten(self,l=0.):
+    def shorten(self,l=0.):
         return SIXD(length=l,label=self.label, particle=self.particle, viseo=self.viseo)
     def adjust_energy(self,tkin):
         self.__init__(length=self.length, viseo=self.viseo, label=self.label, particle=self.particle(tkin), position=self.position)
@@ -74,17 +74,18 @@ class SIXD(ELM.D):
             beta0    = soll.beta
             p0       = soll.p          # cp-soll [MeV]
             m0c2     = soll.e0
-            p        = p0/(1.-dp2pi)
+            p        = p0/(1.-dp2p)
             E        = sqrt(p**2+m0c2**2) #E aus dp2p und p0
             tkin     = E-m0c2
             particle = self.off_soll(tkin=tkin)
             gb       = particle.gamma_beta
-            beta     =     particle.beta
+            beta     = particle.beta
 
             px       = gb*m0c2/E0*xp
             py       = gb*m0c2/E0*yp
+            sigma    = z
             psigma   = ((beta0/beta/(1.-dp2p))-1.)/beta0**2
-            f_track  = np.array([x,px,y,py,z,psigma,T,1.,s,1.])
+            f_track  = np.array([x,px,y,py,sigma,psigma,T,1.,s,1.])
             return f_track
         # conversion RipkenSchmidt (six) ==> T3D
         def six2t3d(i_track):
@@ -112,7 +113,7 @@ class SIXD(ELM.D):
             yp   = py/(gb*m0c2/E0)
             z    = sigma
             dp2p = 1.-beta0/beta/(1.+beta0**2*psigma)
-            f_track = np.array([x,xp,y,yp,x,dp2p,T,1.,s,1.])
+            f_track = np.array([x,xp,y,yp,z,dp2p,T,1.,s,1.])
             return f_track
         # Ripken-Schnidt (six) map
         def rps_map(i_track,l):
@@ -126,7 +127,9 @@ class SIXD(ELM.D):
             T        = i_track[EKOO]
             s        = i_track[SKOO]
 
+            E0       = soll.e
             beta0    = soll.beta
+            m0c2     = soll.e0
             eta      = beta0**2*psigmai
             E        = (1.+eta)*E0
             tkin     = E-m0c2
@@ -142,11 +145,71 @@ class SIXD(ELM.D):
             f_track = np.array([xf,pxf,yf,pyf,sigmaf,psigmaf,T,1.,s,1.])
             return f_track
         ##body
-        six_track_i = t3d2six(i_track)
-        six_track_f = rps_map(six_track_i,self.length)
-        f_track     = six2t3d(six_track_f)
+        f_track     = t3d2six(i_track)
+        f_track     = rps_map(f_track,self.length)
+        f_track     = six2t3d(f_track)
         f_track[SKOO] += self.length
         return f_track
     def soll_map(self,f_track):
         f_track[SKOO] += self.length
         return f_track
+
+def test0():
+    print('-----------------------------Test0---')        
+    header_six=[
+            'x',
+            "px",
+            'y',
+            "py",
+            'sigma',
+            'psigma'
+            ]
+    header_t3d=[
+            'x',
+            "x'",
+            'y',
+            "y'",
+            'z',
+            'dp/p'
+            ]
+
+    tkin0      = 100.            #[MeV]
+    l          = 50.e-3          #[m]
+    sxdrift    = SIXD(length=l,particle=Proton(tkin=tkin0))
+    
+    # x,xp,y,yp,z,dp/p  T3D Kordinaten
+    xi  = yi  = 1.e-3
+    xpi = ypi = 1.e-3
+    zi    = 1.e-3
+    dp2pi = 1.e-2
+    koord0 = np.array([xi,xpi,yi,ypi,zi,dp2pi,0.,1.,0.,1.])
+    row = [[
+            '{:9.6e}'.format(xi),
+            '{:9.6e}'.format(xpi),
+            '{:9.6e}'.format(yi),
+            '{:9.6e}'.format(ypi),
+            '{:9.6e}'.format(zi),
+            '{:9.6e}'.format(dp2pi)
+            ]]
+    print('T3D IN\n'+tblprnt(header_t3d,row))
+    
+    koord = sxdrift.map(koord0)
+    xf    = koord[0]
+    xpf   = koord[1]
+    yf    = koord[2]
+    ypf   = koord[3]
+    zf    = koord[4]
+    dp2pf = koord[5]
+    row = [[
+            '{:9.6e}'.format(xf),
+            '{:9.6e}'.format(xpf),
+            '{:9.6e}'.format(yf),
+            '{:9.6e}'.format(ypf),
+            '{:9.6e}'.format(zf),
+            '{:9.6e}'.format(dp2pf)
+            ]]
+    print('six OUT in T3D Koordinaten\n'+tblprnt(header_t3d,row))
+##main
+if __name__ == '__main__':
+    test0()
+
