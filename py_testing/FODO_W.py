738a06824a5884e536d18c553206526bccf79e00
Wolf-Dieter Klotz <wdklotz@gmail.com>
2018-10-19 18:37:28 +0200

    in work
    *)FODO_W: alpha variation confirms simu plots
    *) new doc/*tex versions

--- py_testing/FODO_W.py
+++ py_testing/FODO_W.py
@@ -8,10 +8,19 @@ from setutil import Twiss
 
 S.init_printing()
 
+def DEBUG_ON(*args):
+    print(args)
+def DEBUG_OFF(*args):
+    pass
+    
+my_debug = DEBUG_ON
+my_debug = DEBUG_OFF
+
 def Mfodo(f,L): # (6.1)   full cell matrix
-    """ Wiedemann corrected
-        L = distance between quad centers
-        f = focus of 1/2 quad
+    """ 
+    Wiedemann corrected
+    L = distance between quad centers
+    f = focus of 1/2 quad
     """
     fstar = 1./(2.*L/f**2*(1-L/f))      # Wiedemann (6.1) is wrong!!
     fstar = 1./(2.*L/f**2*(1+L/f))      # corrected
@@ -46,18 +55,31 @@ def mmult(m1,m2):
             for j in range(2):
                 m1m2[i,k] += (m1[i,j] * m2[j,k])
     return m1m2
-    
-
+def twmatrix(M):
+    C  = M[0][0]
+    S  = M[0][1]
+    CP = M[1][0]
+    SP = M[1][1]
+    m11 = C**2
+    m12 = -2.*S*C
+    m13 = S**2
+    m21 = -C*CP
+    m22 = SP*C+S*CP
+    m23 = -S*SP
+    m31 = CP**2
+    m32 = -2.*SP*CP
+    m33 = SP**2
+    twissmatrix = np.array([[m11,m12,m13],[m21,m22,m23],[m31,m32,m33]])
+    return twissmatrix
 
 # Bg=30.            # [T/m] DB/Dz
 Bg=21.575         # [T/m] DB/Dz
 E0=0.93826        # [GeV] proton rest
 Ql=0.04           # full quad [m]
 R=0.011           # aperture [m]
-NC=100            # NC cells
 
 def test0():
-    """ Wiedemann's fodo Formeln """
+    """ Wiedemann's FODO Formeln """
     print('--------------------------------------------" Wiedemann`s FODO Formeln "')
     print(F'kappa-opt = {M.sqrt(2):8.4g}   phi-opt = {90.} [deg]')
     print('   {T:8s}    {L:8s}  {k:8s}  {beta:8s}    {betap:8s}   {betam:8s}   {kappa:8s}   {phi:8s}'.format(T='T[MeV]',L='L[m]',k='k[m^-2]',beta='beta-kin',betap='beta+', betam='beta-', kappa='kappa', phi='phi [deg]'))
@@ -92,7 +114,10 @@ def test0():
     return
 
 def test1():
-    print('\n\nFODO.......................................FODO')
+    """
+    Wiedemann's Formel hat einen Fehler!
+    """
+    print("\n\n...................",    """Wiedemann's Formel hat einen Fehler!""")
     fs,ld = S.symbols('fs ld')
     QF  = S.Matrix([[1,0],[-1/fs,1]])
     DR  = S.Matrix([[1,ld],[0,1]])
@@ -102,6 +127,7 @@ def test1():
     
     FODO = FODO1*FODO2
     FODO = S.expand(FODO)
+    print('... this is the correct one:')
     S.pprint(FODO)
 
     T = 25.           # kin. energy [MeV]
@@ -115,13 +141,29 @@ def test1():
     fodo1 = FODO.subs(fs,2*f).subs(ld,L/2.)
     fodo1 = np.array(fodo1)
     fodo2 = Mfodo(2*f,L/2.)         # Wiedemann  (the book is wrong!!)
-    print('probe: fodo1-fodo2 must be zero matrix')
+    my_debug('matrix probe: fodo1-fodo2 must be zero matrix')
     zero = fodo1-fodo2
-    print(f'{abs(zero[0][0])}  {abs(zero[0][1])}')
-    print(f'{abs(zero[1][0])}  {abs(zero[1][1])}')
+    my_debug(f'{abs(zero[0][0])}  {abs(zero[0][1])}')
+    my_debug(f'{abs(zero[1][0])}  {abs(zero[1][1])}')
     return
     
 def test2():
+    """
+    To understand and to confirm the correctnes of the SIMULINAC plots,
+    check the influence of twiss-alha parameter using a thin lens FODO.
+    """
+    print('test2 ............................................................',
+    """
+    To understand and to confirm the correctnes of the SIMULINAC plots,
+    this test checks the influence of the twiss-alpha parameter using a 
+    thin lens FODO.
+    """)
+    # initial conditions for twiss parameters
+    beta = 1.          # [m]
+    alfa = -0.5
+    epsi = 1.e-6       # emittance [m*rad]
+    # other initials
+    NC=56             # NC cells
     T = 25.           # kin. energy [MeV]
     T=T*1.e-3         # [GeV] kin.energy 
     betakin=M.sqrt(1.-(1+T/E0)**-2)   # beta kinetic
@@ -143,23 +185,27 @@ def test2():
     trace = abs(mfodo.trace())
     
     mfodo2 = Mfodo(2*f,L/2.)         # Wiedemann
-    print('probe: mfodo-mfodo2 must be zero matrix')
+    my_debug('matrix probe: mfodo-mfodo2 must be zero matrix')
     zero = mfodo-mfodo2
-    print(f'{abs(zero[0][0]):.5f}  {abs(zero[0][1]):.5f}')
-    print(f'{abs(zero[1][0]):.5f}  {abs(zero[1][1]):.5f}')
+    my_debug(f'{abs(zero[0][0]):.5f}  {abs(zero[0][1]):.5f}')
+    my_debug(f'{abs(zero[1][0]):.5f}  {abs(zero[1][1]):.5f}')
     
     # now in slices
-    slices = 100
+    slices = 20
     ld = L/slices
     lq = Ql/slices
     f = 1./(k*lq)
-    md  = Md(ld/2)
-    mqf = Mq(+2*f,lq/2)
-    mqd = Mq(-2*f,lq/2)
-    lattice = []
-    pos = [0.]
-    s = 0.
-    mfodo5 = np.array([[1.,0.],[0.,1.]])
+    md   = Md(ld/2)         # element
+    mqf  = Mq(+2*f,lq/2)    # element
+    mqd  = Mq(-2*f,lq/2)    # element
+    twd  = twmatrix(md)     # twiss-matrix
+    twqf = twmatrix(mqf)    # twiss-matrix
+    twqd = twmatrix(mqd)    # twiss-matrix
+    lattice   = []          # element list
+    twlattice = []          # twiss matrix list
+    pos = [0.]              # position list
+    s = 0.                  # postion (abzsisse)
+    mfodo5 = np.array([[1.,0.],[0.,1.]])# unit matrix
     for m in range(NC):
         mfodo6 = mfodo5.dot(mfodo2)     # chain Wiedemann cells
         mfodo5 = mfodo6
@@ -167,55 +213,80 @@ def test2():
             s += lq
             pos.append(s)
             lattice.append(mqd)
+            twlattice.append(twqd)
         for n in range(1,slices+1):
             s += ld
             pos.append(s)
             lattice.append(md)
+            twlattice.append(twd)
         for n in range(1,slices+1):
             s += lq
             pos.append(s)
             lattice.append(mqf)
+            twlattice.append(twqf)
         for n in range(1,slices+1):
             s += lq
             pos.append(s)
             lattice.append(mqf)
+            twlattice.append(twqf)
         for n in range(1,slices+1):
             s += ld
             pos.append(s)
             lattice.append(md)
+            twlattice.append(twd)
         for n in range(1,slices+1):
             s += lq
             pos.append(s)
             lattice.append(mqd)
+            twlattice.append(twqd)
+    # full lattice matrix
     mfodo3 = np.array([[1.,0.],[0.,1.]])
     for node in lattice:
         mfodo4 = mfodo3.dot(node)
         mfodo3 = mfodo4
-    print('probe: mfodo3-mfodo5 must be zero matrix')
+    my_debug('lattice probe: mfodo3-mfodo5 must be zero matrix')
     zero = mfodo3-mfodo5
-    print(f'{abs(zero[0][0]):.5f}  {abs(zero[0][1]):.5f}')
-    print(f'{abs(zero[1][0]):.5f}  {abs(zero[1][1]):.5f}')
-             
-    beta = 1.          # [m]
-    alfa = 0.
-    epsi = 1.e-6      # emiitance [m*rad]
+    my_debug(f'{abs(zero[0][0]):.5f}  {abs(zero[0][1]):.5f}')
+    my_debug(f'{abs(zero[1][0]):.5f}  {abs(zero[1][1]):.5f}')
     
     twx = Twiss(beta,alfa,epsi)
-    track_point = np.array(twx.y1())    # C track
-    points = [track_point]
-    for node in lattice:
-        tracked = node.dot(track_point)
-        points.append(tracked)
-        track_point = tracked
+    # C track
+    track_point_C = np.array(twx.y1())
+    # S track
+    track_point_S = np.array(twx.y4())
 
-    x  = [v[0] for v in points]
-    xp = [v[1] for v in points]
-    plt.figure('x & xp')
-    plt.plot(pos,x, label='x')
-    plt.plot(pos,xp, label='xp')
-    plt.legend(loc='lower right',fontsize='small')
-    plt.figure('x-xp')
-    plt.plot(x,xp)
+    points_c      = [track_point_C]
+    points_s      = [track_point_S]
+    for node in lattice:
+        C_point = node.dot(track_point_C)
+        points_c.append(C_point)
+        track_point_C = C_point
+        S_point = node.dot(track_point_S)
+        points_s.append(S_point)
+        track_point_S = S_point
+
+    # enveloppe 
+    beta0, alfa0, gamma0 , epsi0 = twx()
+    twiss_point = np.array([beta0,alfa0,gamma0])   # twiss track
+    twpoints = [twiss_point]
+    for bmx in twlattice:
+        twissp = bmx.dot(twiss_point)
+        twpoints.append(twissp)
+        twiss_point = twissp
+
+    # trajectories
+    c_trk  = [v[0] for v in points_c]
+    s_trk  = [v[0] for v in points_s]
+    # enveloppe  (beta*emittance)^(1/2)
+    sgx = [M.sqrt(v[0]*epsi0) for v in twpoints]
+    
+    plt.figure('x & sgx')
+    ax1 = plt.subplot(111)
+    ax1.set_title(F'alpha = {alfa:2.1f}')
+    ax1.plot(pos,c_trk,   label='C')
+    ax1.plot(pos,s_trk,   label='S')
+    ax1.plot(pos,sgx,     label='env')
+    ax1.legend(loc='lower right',fontsize='small')
     plt.show()
     return
     
