be62777a6dab058be18cf7ffb39cc44a58a21e02
wdklotz <wdklotz@gmail.com>
2019-06-28 00:30:07 +0200

    pandasKW1.py

---
+++ py_testing/pandasKW1.py
@@ -0,0 +1,26 @@
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+from math import pi,degrees,sin,cos
+
+columns = ["x","xp","y","yp","z","zp","T",1,"S","1"]
+
+n = 100
+p=[]
+index=[]
+for i in range(n+1):
+	arg = 2*pi*i/n
+	x=sin(arg)
+	xp=cos(arg)
+	y=sin(2*arg)
+	yp=cos(2*arg)
+	p.append([x,xp,y,yp,0.,0.,0.,1.,0.,1.])
+	index.append(arg)
+
+df=pd.DataFrame(p,columns=columns,index=index)
+df.loc[:,["x","xp","y","yp"]].plot()
+pl.show()
+
+
