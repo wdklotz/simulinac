e3bb50c24ae5daf74966663c17990698ea6d8d8c
Wolf-Dieter Klotz <wdklotz@gmail.com>
2021-11-18 15:15:26 +0100

    update version number and compatIN

--- tracker.py
+++ tracker.py
@@ -1,6 +1,6 @@
 #!/Users/klotz/anaconda3/bin/python3.6
 # -*- coding: utf-8 -*-
-___version___='v10.0.0'
+___version___='v10.0.1'
 """
 Copyright 2015 Wolf-Dieter Klotz <wdklotz@gmail.com>
 This file is part of the SIMULINAC code
