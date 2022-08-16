4efd45e480a67381a3b14e9976076463525938e7
Wolf-Dieter Klotz <wdklotz@gmail.com>
2022-08-16 12:12:54 +0200

    renamed/moved directories SuperFish, xml_utils

--- xml_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-## namespace xml_utils
-## xml Utility classes.
-##
-## Classes:
-## - XmlDataAdaptor   - Class. A DataAdaptor that supports reading/writing XML
-## - NamedObject      - Class. Represents an object with a name.
-## - TypedObject      - Class. Represents an object with a type.
-## - ParamsDictObject - Class. Represents an object that has a parameters dictionary.
