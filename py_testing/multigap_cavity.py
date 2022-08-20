a058eccdb793fe15edc6d29bacfd57ee4c6b36a1
Wolf-Dieter Klotz <wdklotz@gmail.com>
2022-08-20 08:53:43 +0200

    * removed some old files from workspace * added jupyter notebook file * added a new full energy run (5-200 MeV) compatible with simu.py   v10.1.0: yml/I5O200_19082022.yml

--- py_testing/multigap_cavity.py
+++ py_testing/multigap_cavity.py
@@ -6,18 +6,18 @@ import scipy.constants as C
 
 pi      = M.pi
 twopi   = pi+pi
-threepi = twopi+pi
-fourpi  = threepi+pi
+# threepi = twopi+pi
+# fourpi  = threepi+pi
  
-cl    = C.c
-freq  = 816e6
-lamb  = cl/freq
-omega = twopi*freq
-m0c2  = C.value('proton mass energy equivalent in MeV')
-tk    = 50   # kin. energy
-gamma =  1.+tk/m0c2
-beta  = M.sqrt(1. - 1./gamma**2)
-blamb = beta*lamb
+# cl    = C.c
+# freq  = 816e6
+# lamb  = cl/freq
+# omega = twopi*freq
+# m0c2  = C.value('proton mass energy equivalent in MeV')
+# tk    = 50   # kin. energy
+# gamma =  1.+tk/m0c2
+# beta  = M.sqrt(1. - 1./gamma**2)
+# blamb = beta*lamb
 
 # print(" \
 #     c-light [m/s]....... {0:G} \n \
@@ -36,51 +36,50 @@ def generate_space_part(delta=0.0):
     gn,gsig     = (5,  0.125)   # nb gaps, gap sigma
     glen        = 1.0           # interval length
     gstart      = -glen/2.
-    gend        = +gstart
-    glen        = glen*(1.0+delta*1e-2)         # interval length
     # concatenate several gaps
     y = NP.array([])
     x = NP.array([])
+    xstart = gstart
     for n in range(gn):
-        xstart = gstart + n*glen
-        xstop  = xstart + glen
+        xstop  = xstart + glen*(1.+delta/100.)
         xrange = NP.linspace(xstart, xstop, 50)
-        xpos   = (xstop + xstart)/2.
+        xpos   = (xstart+glen/2.)
         yy     = M.sqrt(twopi)*gsig * norm.pdf(xrange,xpos,gsig)
         x      = NP.concatenate((x,xrange))
         y      = NP.concatenate((y,yy))        # space part
+        xstart = xstop
     return x,y
 
 phi = lambda u: twopi*u+p0+pi/2.0
 
 fig = PLT.figure(figsize=(14.,6.))      # prep plotting
 
-phisoll = -45.0
+phisoll = [-45.0,-90.0,-0.0]
 x,y = generate_space_part(delta=0.0)    # gap positions
 splt = PLT.subplot(211)
 PLT.title(r'5 gaps with spacing = $\beta\lambda$')
 PLT.plot(x,y,':')
-for p in [-90.,phisoll,0.]:
+for p in phisoll:
     p0 = M.radians(p)
     Eup = NP.array([])
     for i,u in enumerate(x):
         v = M.sin(phi(u))*y[i]
         Eup = NP.append(Eup,[v])
-    fmt = 'o' if p == phisoll else '-'
+    fmt = 'ok' if p == phisoll[0] else '-'
     PLT.plot(x,Eup,fmt,label=f'{p:+3.1f} deg',markersize=3)
 PLT.legend()
 
-x,y = generate_space_part(delta=10.0)    # gap positions
+x,y = generate_space_part(delta=+10.0)    # gap positions
 splt = PLT.subplot(212)
 PLT.title(r'5 gaps with spacing = $\beta\lambda$ + 10%')
 PLT.plot(x,y,':')
-for p in [-90.,phisoll,0.]:
+for p in phisoll:
     p0 = M.radians(p)
     Eup = NP.array([])
     for i,u in enumerate(x):
         v = M.sin(phi(u))*y[i]
         Eup = NP.append(Eup,[v])
-    fmt = 'o' if p == phisoll else '-'
+    fmt = 'ok' if p == phisoll[0] else '-'
     PLT.plot(x,Eup,fmt,label=f'{p:+3.1f} deg',markersize=3)
 PLT.legend()
 splt.set_xlabel(r'$\beta\lambda$')
