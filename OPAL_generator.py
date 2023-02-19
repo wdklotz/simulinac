3ecc4960f702ca9df22a940b185c4e5f4605ff3e
Wolf-Dieter Klotz <wdklotz@gmail.com>
2023-02-19 11:48:24 +0100

    v10.23.1: NEW version due to many small impovements and correction in previous version. 1st version in 2023.

--- OPAL_generator.py
+++ OPAL_generator.py
@@ -1,4 +1,4 @@
-__version__='v10.22.7'
+__version__='v10.23.1'
 """ 
 DYNAC_lattice-generator
 converts YAML input file (simuIN.yml) into dynacIN for dynacv7r1.
--- elements.py
+++ elements.py
@@ -1,4 +1,4 @@
-__version__='v10.22.7'
+__version__='v10.23.1'
 """
 Copyright 2015 Wolf-Dieter Klotz <wdklotz@gmail.com>
 This file is part of the SIMULINAC code
