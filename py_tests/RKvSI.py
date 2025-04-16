e66cd45c18e6bed7ca20aff613af46a06a2bb69d
W.D.Klotz <wdklotz@gmail.com>
2025-04-16 23:53:10 +0200

    no comment

--- py_tests/RKvSI.py
+++ py_tests/RKvSI.py
@@ -1,22 +1,4 @@
-#!/Users/klotz/SIMULINAC_env/bin/python
 # -*- coding: utf-8 -*-
-"""
-Copyright 2015 Wolf-Dieter Klotz <wdklotz@gmail.com>
-This file is part of the SIMULINAC code
-
-    SIMULINAC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    SIMULINAC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with SIMULINAC.  If not, see <http://www.gnu.org/licenses/>.
-"""
 import matplotlib.pyplot as plt
 from math import cos,sin,radians,degrees
 
